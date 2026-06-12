15c991515e60080f4735d05b03380df4e469aed0
Michael Gunkoff <kaatinga@gmail.com>
2026-06-12 16:12:19 +0200

    explort Option

--- options.go
+++ options.go
@@ -14,9 +14,10 @@ type options struct {
 	port         string // Host port for container
 }
 
-type option func(*options)
+// Option configures a container helper created by NewPostgres, NewRedis or NewNats.
+type Option func(*options)
 
-func (o *options) applyOptions(opts []option) {
+func (o *options) applyOptions(opts []Option) {
 	o.extraEnvVars = make(map[string]string)
 	for _, opt := range opts {
 		opt(o)
@@ -24,7 +25,7 @@ func (o *options) applyOptions(opts []option) {
 }
 
 // WithCustomImage sets a custom Docker image and version for the container.
-func WithCustomImage(image, version string) option {
+func WithCustomImage(image, version string) Option {
 	return func(opt *options) {
 		opt.image = image
 		opt.version = version
@@ -32,14 +33,16 @@ func WithCustomImage(image, version string) option {
 }
 
 // WithNetwork sets a custom Docker network for the container to join.
-func WithNetwork(network *testcontainers.DockerNetwork) option {
+func WithNetwork(network *testcontainers.DockerNetwork) Option {
 	return func(opt *options) {
 		opt.network = network
 	}
 }
 
 // WithPort sets the host port for the container port binding.
-func WithPort(port string) option {
+// An empty string lets Docker pick a random free port; the actual port is
+// available via the service's Port() method after Start.
+func WithPort(port string) Option {
 	return func(opt *options) {
 		opt.port = port
 	}
@@ -47,7 +50,7 @@ func WithPort(port string) option {
 
 // WithEnvVars adds custom environment variables to the container.
 // Multiple calls to WithEnvVars will merge the environment variables.
-func WithEnvVars(vars map[string]string) option {
+func WithEnvVars(vars map[string]string) Option {
 	return func(opt *options) {
 		maps.Copy(opt.extraEnvVars, vars)
 	}
